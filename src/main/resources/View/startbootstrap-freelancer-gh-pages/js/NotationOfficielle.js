aa187db69678d776c94742765cd1be51eea2f396
Lara <laranassim24@gmail.com>
2020-12-18 15:52:10 +0100

    avancement projet ajouts mise en place correct du back à corriger encore pour la bdd

---
+++ src/main/resources/View/startbootstrap-freelancer-gh-pages/js/NotationOfficielle.js
@@ -0,0 +1,9 @@
+
+document.getElementById("etoiles").addEventListener("click", lorsquejeclique);
+
+function lorsquejeclique() {
+    document.getElementById("etoiles").innerHTML = "Merci de votre attention!";
+}
+
+
+
